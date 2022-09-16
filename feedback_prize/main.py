b70e69423751f5fa3bdc7e7aa141429ceacb238f
ktagashira <k-tagashira@wevnal.co.jp>
2022-09-16 18:52:59 +0900

    chore: add .gitignore, .env

--- feedback_prize/main.py
+++ feedback_prize/main.py
@@ -1,3 +1,21 @@
+import os
+from pathlib import Path
+from utils import is_script_running
+from dotenv import load_dotenv
+
+import pandas as pd
+
+load_dotenv()
+
+ON_KAGGLE: bool = is_script_running()
+DATA_ROOT = Path(os.path.join(
+    '../input', os.environ['PROJECT']) if ON_KAGGLE else os.path.join('/content', os.environ['PROJECT']))
+
+train = pd.read_csv(DATA_ROOT / 'train.csv')
+test = pd.read_csv(DATA_ROOT / 'test.csv')
+sample_submission = pd.read_csv(DATA_ROOT / 'sample_submission.csv')
+
+
 def main():
     print('Hello world!')
 
