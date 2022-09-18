78fa2c37bf593ccb66c7b6c9aa3366bc1f89adcc
ktagashira <k-tagashira@wevnal.co.jp>
2022-09-18 23:22:56 +0900

    chore: add load_dotenv in train.py

--- feedback_prize/train.py
+++ feedback_prize/train.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import torch
+from dotenv import load_dotenv
 from torch import nn
 from torch.optim import AdamW
 from torch.utils.data import DataLoader, Dataset
@@ -17,6 +18,7 @@ from model import CustomModel
 from utils import (AverageMeter, get_score, is_script_running, seed_everything,
                    timeSince)
 
+load_dotenv()
 seed_everything(42)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 OUTPUT_DIR = os.path.join(CFG.colab_path, 'OUTPUT')
